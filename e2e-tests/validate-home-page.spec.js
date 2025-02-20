6de2afd5689e83af23cbe8fcc5577d52a7f7db99
PrakGov <55034062+prak112@users.noreply.github.com>
2025-02-20 17:39:18 +0200

    exercises: 11.9-Update tests to use relative URLs

--- e2e-tests/validate-home-page.spec.js
+++ e2e-tests/validate-home-page.spec.js
@@ -4,31 +4,31 @@ const { test, describe, expect } = require('@playwright/test')
 
 describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
-    await page.goto('http://localhost:8081/')
+    await page.goto('/')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
 
   test('can navigate to a specific Pokémon page', async ({ page }) => {
-    await page.goto('http://localhost:8081/')
+    await page.goto('/')
     await page.click('text=ivysaur')
-    await expect(page).toHaveURL('http://localhost:8081/pokemon/ivysaur')
+    await expect(page).toHaveURL('/pokemon/ivysaur')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Previous')).toBeVisible()
     await expect(page.getByText('Next')).toBeVisible()
   })
 
   test('can navigate to the next Pokémon page', async ({ page }) => {
-    await page.goto('http://localhost:8081/pokemon/ivysaur')
+    await page.goto('/pokemon/ivysaur')
     await page.click('text=Next')
-    await expect(page).toHaveURL('http://localhost:8081/pokemon/venusaur')
+    await expect(page).toHaveURL('/pokemon/venusaur')
     await expect(page.getByText('venusaur')).toBeVisible()
   })
 
   test('can navigate to the previous Pokémon page', async ({ page }) => {
-    await page.goto('http://localhost:8081/pokemon/ivysaur')
+    await page.goto('/pokemon/ivysaur')
     await page.click('text=Previous')
-    await expect(page).toHaveURL('http://localhost:8081/pokemon/bulbasaur')
+    await expect(page).toHaveURL('/pokemon/bulbasaur')
     await expect(page.getByText('bulbasaur')).toBeVisible()
   })
 })
