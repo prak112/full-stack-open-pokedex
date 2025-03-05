27d5e76d2856e91e7b8f823007635e1a2edeb69b
PrakGov <55034062+prak112@users.noreply.github.com>
2025-03-05 16:40:43 +0200

    feat: ex 11.12-setup health check endpoint

--- app.js
+++ app.js
@@ -4,8 +4,26 @@ const app = express()
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
+// health check endpoint
+app.get('/health', (req, res) => {
+  const healthCheck = {
+    service: 'pokedex',
+    uptime: process.uptime(),
+    status: 'ok',
+    timestamp: new Date().toISOString()
+  }
+  try {
+    res.json(healthCheck)
+  } catch (error) {
+    healthCheck.status = 'error'
+    healthCheck.message = error.message
+    res.status(503).json(healthCheck)
+  }
+})
+
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
+
